aad74279437e5b16cf0a0cd15d75d6e12c49e5a9
hamza Tanji Cherkaoui <hoxa1235@gmail.com>
2017-09-28 17:26:52 +0100

    adding the todos service

---
+++ src/app/todo.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+
+@Injectable()
+export class TodoService {
+
+  constructor() { }
+
+  getTodos(){
+  	return Observable.timer(2000)
+  	.mapTo ([
+  		{id:1,title : "Learn ngrx/store" , completed : false},
+  		 {id:2,title : "Learn ngrx/effects" , completed : false}
+  		 ])
+  }
+}
